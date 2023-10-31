d20551b04dc710793cf9671bd622dc4ad3df1529
Xiaok29 <1526783667@qq.com>
2023-10-31 16:34:33 +0800

    feat:做题

--- exercises/07_structs/structs3.rs
+++ exercises/07_structs/structs3.rs
@@ -31,12 +31,13 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> ??? {
+    fn is_international(&self) -> bool {
         // Something goes here...
+        self.sender_country != self.recipient_country
     }
 
-    fn get_fees(&self, cents_per_gram: u32) -> ??? {
-        // Something goes here...
+    fn get_fees(&self, cents_per_gram: u32) -> i32 {
+        (self.weight_in_grams * cents_per_gram).try_into().unwrap()
     }
 }
 
